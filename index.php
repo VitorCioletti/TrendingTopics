94ff219ebe3f128da4e867c2e83ffeaebc67c53d
VitorCioletti <ciolettiv@gmail.com>
2017-06-29 15:47:40 -0300

    Testing new functions

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 include 'TwitterAPI.php';
 $twitterCon = TwitterAPI::getInstance();
 #$twitterCon->trendsPlace("Brazil");
-$twitterCon->userTweets("jovemnerd");
+#$twitterCon->userTweets("jovemnerd");
+$twitterCon->trendTweets($twitterCon->trendsPlace("Brazil"));
