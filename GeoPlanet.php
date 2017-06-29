04344fa8ed1c1acfa46a138ce458e071035c7490
VitorCioletti <ciolettiv@gmail.com>
2017-06-29 20:48:48 -0300

    Added safety swithces and code testing

--- GeoPlanet.php
+++ GeoPlanet.php
@@ -6,10 +6,14 @@ class GeoPlanet {
      * Gets WOEID of a determined country
      * @param $placeName Name of the country.
      */
+    public function __construct() {
+
+    }
+
     public function getWoeid($placeName) {
         $url = "http://query.yahooapis.com/v1/public/yql?q=select%20woeid%20from%20geo.places%20where%20text%3D%22Place%20$placeName%22&format=json";
         $list = json_decode(file_get_contents($url), true);
-        return($list['query']['results']['place'][0]); //Give attention to this expression. It does not seems good. Study more.
+        return($list['query']['results']['place'][0]['woeid']); //Give attention to this expression. It does not seems good. Study more.
     }
 
 }
--- index.php
+++ index.php
@@ -4,4 +4,4 @@ include 'TwitterAPI.php';
 $twitterCon = TwitterAPI::getInstance();
 #$twitterCon->trendsPlace("Brazil");
 #$twitterCon->userTweets("jovemnerd");
-$twitterCon->trendTweets($twitterCon->trendsPlace("Brazil"));
+$twitterCon->trendsPlace("Brazil");
